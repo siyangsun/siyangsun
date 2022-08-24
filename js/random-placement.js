1e566f75ab5fe3316bd3d7e38d5120af51f1f649
siyangsun <siyangsun2007@gmail.com>
2022-08-24 10:09:07 -0700

    rename js with - instead of _

--- js/random-placement.js
+++ js/random-placement.js
@@ -2,7 +2,12 @@
 var stars = document.getElementsByTagName('star');
 // get window width and height
 var winWidth = window.innerWidth;
+
 var winHeight = window.innerHeight;
+// function that returns a random number between a min and max
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 // i stands for "index". you could also call this banana or haircut. it's a variable
 for (var i = 0; i < stars.length; i++) {
@@ -12,16 +17,9 @@ for (var i = 0; i < stars.length; i++) {
 
   // get random numbers for each element
   randomTop = getRandomNumber(0, winHeight);
-  randomLeft = getRandomNumber(0, winWidth) + 50;
+  randomLeft = getRandomNumber(0, winWidth);
 
   // update top and left position
   thisStar.style.top = randomTop + "px";
   thisStar.style.left = randomLeft + "px";
-}
-
-// function that returns a random number between a min and max
-function getRandomNumber(min, max) {
-
-  return Math.random() * (max - min) + min;
-
 }
