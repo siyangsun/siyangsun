af3ab8696b414342939aa63c22370b9b33a75dbf
siyangsun <siyangsun2007@gmail.com>
2022-08-23 21:24:42 -0700

    rename variables in js

--- js/random_placement.js
+++ js/random_placement.js
@@ -1,23 +1,22 @@
-// collect all the divs
-var divs = document.getElementsByTagName('star');
+// collect all the stars
+var stars = document.getElementsByTagName('star');
 // get window width and height
 var winWidth = window.innerWidth;
 var winHeight = window.innerHeight;
 
 // i stands for "index". you could also call this banana or haircut. it's a variable
-for (var i = 0; i < divs.length; i++) {
+for (var i = 0; i < stars.length; i++) {
 
-  // shortcut! the current div in the list
-  var thisDiv = divs[i];
+  // shortcut! the current star in the list
+  var thisStar = stars[i];
 
   // get random numbers for each element
   randomTop = getRandomNumber(0, winHeight);
   randomLeft = getRandomNumber(0, winWidth);
 
   // update top and left position
-  thisDiv.style.top = randomTop + "px";
-  thisDiv.style.left = randomLeft + "px";
-
+  thisStar.style.top = randomTop + "px";
+  thisStar.style.left = randomLeft + "px";
 }
 
 // function that returns a random number between a min and max
